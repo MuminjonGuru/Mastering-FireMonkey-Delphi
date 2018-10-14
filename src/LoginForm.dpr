c0531943d2abf4ef2de553678798a9d8bbfb119c
Abduraimov Mo'minjon <24930020+MuminjonGuru@users.noreply.github.com>
2018-10-15 00:04:09 +0500

    Add files via upload

---
+++ src/LoginForm.dpr
@@ -0,0 +1,14 @@
+program LoginForm;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  uMain in 'uMain.pas' {FrmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TFrmMain, FrmMain);
+  Application.Run;
+end.
