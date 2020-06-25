b172058dbe4d7423d57fb3963895998dbc7aa6f2
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-06-25 23:40:37 +0500

    update

--- Client Controller Application Prototype/uMain.pas
+++ Client Controller Application Prototype/uMain.pas
@@ -1,11 +1,27 @@
+{**************************************************}
+{   Muminjon.com - Delphi.uz                       }
+{                    @MuminjonGuru                 }
+{**************************************************}
+
 unit uMain;
 
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
-  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts;
+  System.SysUtils
+, System.Types
+, System.UITypes
+, System.Classes
+, System.Variants
+, FMX.Types
+, FMX.Controls
+, FMX.Forms
+, FMX.Graphics
+, FMX.Dialogs
+, FMX.Objects
+, FMX.Controls.Presentation
+, FMX.StdCtrls
+, FMX.Layouts;
 
 type
   TFormMain = class(TForm)
